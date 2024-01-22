e680202e83d08d385571c5160b6d6d6dcd700834
Jesulayomy <jesulayomi081@gmail.com>
2024-01-22 10:57:07 +0100

    [Feature]: Custom error pages

--- lushlyrics-webapp-django-main/main/urls.py
+++ lushlyrics-webapp-django-main/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("", views.default, name='default'),
     path("playlist/", views.playlist, name='your_playlists'),
